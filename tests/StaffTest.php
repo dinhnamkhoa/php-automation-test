59057401706fb008c7b565ca38c0b0a3db123d5b
dinhnamkhoa <dinhnamkhoa@gmail.com>
2022-03-02 15:01:51 +0000

    Push

---
+++ tests/StaffTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class StaffTest extends TestCase
+{
+	public function testReturnsFullName()
+	{
+		require('Staff.php');
+
+		$staff = new Staff;
+
+		$staff->first_name = "Dinh Nam";
+		$staff->last_name = "Khoa";
+
+		$this->assertEquals('Dinh Nam Khoa', $staff->getFullName());
+	}
+	public function testFullNameIsEmptyByDefault()
+	{
+		$staff = new Staff;
+		$this->assertEquals('', $staff->getFullName());
+	}
+
+	/**
+	 * @test
+	 */
+	public function only_first_name()
+	{
+		$staff = new Staff;
+		$staff->first_name ="Dinh";
+		$this->assertEquals('Dinh Nam', $staff->getFullName());
+	}
+}
